d1aaa604631c33295e8e0305d725cfa3b9216dea
montiho8 <montiho8@hawaii.edu>
2022-09-28 23:06:29 -1000

    Initial Commit

---
+++ AppBuildOne/AppBuildOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBuildOneApp.swift
+//  AppBuildOne
+//
+//  Created by Dominique Cortez-Montiho on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AppBuildOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
